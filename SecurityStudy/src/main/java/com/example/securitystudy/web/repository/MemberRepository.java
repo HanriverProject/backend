3b976f45dfc4ee6cdbaf4ad06e524ee2725b0813
KangWookOh <49800255+KangWookOh@users.noreply.github.com>
2023-05-26 12:56:06 +0900

    Delete SecurityStudy directory

--- SecurityStudy/src/main/java/com/example/securitystudy/web/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securitystudy.web.repository;
-
-import com.example.securitystudy.web.entity.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Long> {
-
-}
